905aeda7b62a8623baba74a1500a143e75d2d65f
Javier Mantilla <javier.mantilla@synchronoss.com>
2017-12-01 18:33:22 +0000

    Initial commit

---
+++ G00329649/src/com/geog/model/Country.java
@@ -0,0 +1,43 @@
+package com.geog.model;
+
+public class Country {
+//	Instance variables
+	private String code;
+	private String name;
+	private StringBuilder details;
+	
+	
+	
+	
+//	Constructor
+	public Country() {
+	}
+
+
+
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public StringBuilder getDetails() {
+		return details;
+	}
+
+	public void setDetails(StringBuilder details) {
+		this.details = details;
+	}
+
+} // class Country
---
+++ G00329649/src/com/geog/model/Region.java
@@ -0,0 +1,5 @@
+package com.geog.model;
+
+public class Region {
+
+}
